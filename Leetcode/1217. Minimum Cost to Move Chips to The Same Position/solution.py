da6ec0ed370e2431ab87f4c008e96aa8b2fa293e
CasoMateo <67202211+CasoMateo@users.noreply.github.com>
2021-05-03 08:49:22 -0500

    Create solution.py

---
+++ Leetcode/1217. Minimum Cost to Move Chips to The Same Position/solution.py
@@ -0,0 +1,9 @@
+
+class Solution:
+    def minCostToMoveChips(self, chips: List[int]) -> int:
+        even = 0
+        for chip in chips:
+            if chip % 2 == 0:
+                even += 1
+
+        return min(even, len(chips) - even)
