c4ddd05eea155919e663122de864455828786641
CasoMateo <67202211+CasoMateo@users.noreply.github.com>
2021-05-23 17:37:35 -0500

    Update solution.py

--- Leetcode/34. Find First and Last Position of Element in Sorted Array/solution.py
+++ Leetcode/34. Find First and Last Position of Element in Sorted Array/solution.py
@@ -5,25 +5,21 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+        if target not in nums:
+          return [-1, -1]
+    
         if nums.count(target) == 1:
           return [nums.index(target), nums.index(target)]
           
-  
-        seen = set()
         for i in range(len(nums)):
           if nums[i] == target: 
-            if nums[i] not in seen:
-              i1 = i
-              seen.add(nums[i])
-              break
-      
+            i1 = i
+            break
+            
         for j in range(len(nums)):
           if nums[len(nums) - j - 1] == target: 
             i2 = len(nums) - j - 1
             break
-            
-        if target not in nums:
-          return [-1, -1]
         return [i1, i2]
       
       
