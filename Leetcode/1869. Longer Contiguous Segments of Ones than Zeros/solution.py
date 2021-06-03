ffdadce3541b8f9668d3dc3eaea88922938c429c
CasoMateo <67202211+CasoMateo@users.noreply.github.com>
2021-06-03 16:57:54 -0500

    Update solution.py

--- Leetcode/1869. Longer Contiguous Segments of Ones than Zeros/solution.py
+++ Leetcode/1869. Longer Contiguous Segments of Ones than Zeros/solution.py
@@ -3,9 +3,8 @@ class Solution:
       if len(s) == 1: 
         return True if s[0] == '1' else False
       
-      max_0 = max_1 = 0 
-      cur = 0
-      
+      max_0 = max_1 = cur = 0 
+   
       for i in range(1, len(s)):
         if s[i - 1] == '0':
           if s[i - 1] == s[i]:
