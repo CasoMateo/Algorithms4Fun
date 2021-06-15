401df88860f2f7ba058eac379f46db20620d2c43
CasoMateo <67202211+CasoMateo@users.noreply.github.com>
2021-06-15 09:51:13 -0500

    Update solution.py

--- Leetcode/1002. Find Common Characters/solution.py
+++ Leetcode/1002. Find Common Characters/solution.py
@@ -3,15 +3,15 @@ class Solution:
         
         res = []
         
-        for i in range(len(words[0])):
-          cur = words[0][i]
-          common = 0 
+        for letter in words[0]:
+          common = True
           for j in range(1, len(words)):
-            if cur in words[j]:
-              common += 1 
-              words[j] = words[j][:words[j].find(cur)] + words[j][words[j].find(cur) + 1:]
+            if letter not in words[j]:
+              common = False
+            else:
+              words[j] = words[j].replace(letter, '', 1)
               
-          if common == len(words) - 1:
-            res.append(cur)
+          if common:
+            res.append(letter)
       
         return res
