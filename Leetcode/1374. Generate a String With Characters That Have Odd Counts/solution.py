98f7be923c8912d7a001d3723fce512003cafc72
CasoMateo <67202211+CasoMateo@users.noreply.github.com>
2021-06-29 10:50:21 -0500

    Create solution.py

---
+++ Leetcode/1374. Generate a String With Characters That Have Odd Counts/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def generateTheString(self, n: int) -> str:
+    
+      if n % 2 == 0: 
+        return 'a' * (n - 1) + 'b'
+      else: 
+        return 'a' * n
+
+        
+        
