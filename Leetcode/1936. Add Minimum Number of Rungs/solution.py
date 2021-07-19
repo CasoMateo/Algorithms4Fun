bbb776822138ffa3ecfbf0618b05e89d38ef4dab
CasoMateo <67202211+CasoMateo@users.noreply.github.com>
2021-07-19 11:56:59 -0500

    Update solution.py

--- Leetcode/1936. Add Minimum Number of Rungs/solution.py
+++ Leetcode/1936. Add Minimum Number of Rungs/solution.py
@@ -2,14 +2,11 @@ class Solution:
     def addRungs(self, rungs: List[int], dist: int) -> int:
     
       cur = needed = i = 0 
-    
-      while i < len(rungs):
-        if rungs[i] - cur <= dist: 
-          cur = rungs[i]
-        else: 
-          needed += (rungs[i] - cur - 1) // dist 
-          cur = rungs[i] 
-        i += 1 
         
+      for i in range(len(rungs)):
+        if rungs[i] - cur > dist: 
+          needed += (rungs[i] - cur - 1) // dist
+        cur = rungs[i]
+          
       return needed
         
