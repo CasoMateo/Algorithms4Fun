00d0e8668c480291c9854c6e813e2dbdd45d6524
CasoMateo <67202211+CasoMateo@users.noreply.github.com>
2021-07-21 08:30:40 -0500

    Create solution.py

---
+++ Leetcode/1920. Build Array from Permutation/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+      permutation = []
+      
+      for i in range(len(nums)):
+        permutation.append(nums[nums[i]]) 
+      
+      return permutation
+        
