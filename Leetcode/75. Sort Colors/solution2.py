9364c2682bd218848125dd58289e25a14ff27afb
CasoMateo <67202211+CasoMateo@users.noreply.github.com>
2021-08-06 09:52:28 -0500

    Update solution2.py

--- Leetcode/75. Sort Colors/solution2.py
+++ Leetcode/75. Sort Colors/solution2.py
@@ -4,28 +4,32 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         
-        mem = {}
+        freq_0 = freq_1 = freq_2 = 0 
         
-        for num in nums: 
-          mem[num] = mem.get(num, 0) + 1 
+        if 0 in nums: 
+          freq_0 = nums.count(0)
+        if 1 in nums:
+          freq_1 = nums.count(1)
+        if 2 in nums:
+          freq_2 = nums.count(2)
+          
+        cur = 0 
         
-        if 0 in mem: 
-          nums[: mem[0]] = [0] * mem[0]
+        if freq_0 != 0: 
+          for times in range(freq_0):
+            nums[cur] = 0 
+            cur += 1
           
-        if 1 in mem:
-          if 0 in mem: 
-            nums[mem[0] : mem[0] + mem[1]] = [1] * mem[1] 
-          else: 
-            nums[: mem[1]] = [1] * mem[1]
+        if freq_1 != 0:
+          for times in range(freq_1):
+            nums[cur] = 1
+            cur += 1
           
-        if 2 in mem: 
-          if 0 in mem and 1 in mem: 
-            nums[mem[0] + mem[1]: mem[0] + mem[1] + mem[2]] = [2] * mem[2]
-          elif 0 in mem: 
-            nums[mem[0] : mem[0] + mem[2]] = [2] * mem[2] 
-          elif 1 in mem: 
-            nums[mem[1] : mem[1] + mem[2]] = [2] * mem[2] 
-          else: 
-            nums[: mem[2]] = [2] * mem[2]
-            
+        if freq_2 != 0: 
+          for times in range(freq_2):
+            nums[cur] = 2
+            cur += 1
+          
+      
+        
           
