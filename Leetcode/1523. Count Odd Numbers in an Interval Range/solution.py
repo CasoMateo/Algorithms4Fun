57cf9a5953a137366e9c45e0b3760db1d00c8e63
CasoMateo <67202211+CasoMateo@users.noreply.github.com>
2021-09-11 19:17:09 -0500

    Create solution.py

---
+++ Leetcode/1523. Count Odd Numbers in an Interval Range/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countOdds(self, low: int, high: int) -> int:
+      
+      if low % 2 == 0: 
+        return (high - low + 1) // 2 
+      
+      return 1 + (high - low) // 2
+        
