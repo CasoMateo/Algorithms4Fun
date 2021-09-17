809a448be7d74798a470ced4ecf4d99de15f7739
CasoMateo <67202211+CasoMateo@users.noreply.github.com>
2021-09-17 10:17:46 -0500

    Update solution.py

--- Leetcode/1071. Greatest Common Divisor of Strings/solution.py
+++ Leetcode/1071. Greatest Common Divisor of Strings/solution.py
@@ -1,21 +1,12 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
+     
+        longest = cur = ''
         
-        divisors = []
-        
-        if len(str1) > len(str2):
-          str1, str2 = str2, str1
-          
-        cur = ''
         for i in range(len(str1)):
           cur += str1[i]
           
-          if cur * (len(str1) // len(cur)) == str1:
-            divisors.append(cur)
+          if cur * (len(str1) // len(cur)) == str1 and cur * (len(str2) // len(cur)) == str2:
+            longest = cur
         
-        for greatest in range(len(divisors)):
-          ac = divisors[len(divisors) - greatest - 1] 
-          if ac * (len(str1) // len(ac)) == str1 and ac * (len(str2) // len(ac)) == str2:
-            return ac 
-        
-        return ''
+        return longest
