335f425a83f3be373e5aeebd93a20019359d0f08
CasoMateo <67202211+CasoMateo@users.noreply.github.com>
2021-09-24 13:57:36 -0500

    Create solution.py

---
+++ Leetcode/575. Distribute Candies/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def distributeCandies(self, candyType: List[int]) -> int:
+        
+        if len(set(candyType)) < len(candyType) // 2: 
+          return len(set(candyType))
+        
+        return len(candyType) // 2
