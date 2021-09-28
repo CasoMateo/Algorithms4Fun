bddb9f4719f62bf57d0d600e9de4de3b1920a050
CasoMateo <67202211+CasoMateo@users.noreply.github.com>
2021-09-28 16:50:05 -0500

    Create solution.py

---
+++ Leetcode/414. Third Maximum Number/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+      
+      if len(set(nums)) < 3: 
+        return max(nums)
+      
+      return sorted(set(nums))[-3]
+        
