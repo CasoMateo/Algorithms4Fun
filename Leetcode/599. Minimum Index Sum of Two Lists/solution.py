20b93d2982edbf6b0a3e49962f90b4beb0d35d07
CasoMateo <67202211+CasoMateo@users.noreply.github.com>
2021-09-28 15:24:50 -0500

    Create solution.py

---
+++ Leetcode/599. Minimum Index Sum of Two Lists/solution.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
+        
+        mem = {}
+        
+        for i in range(len(list2)):
+          mem[list2[i]] = i 
+        
+        least = []
+        cur = float('inf')
+        
+        for j in range(len(list1)):
+          if list1[j] in list2: 
+            if mem[list1[j]] + j < cur:
+              least = []
+              least.append(list1[j])
+              cur = mem[list1[j]] + j
+            
+            elif mem[list1[j]] + j == cur: 
+              least.append(list1[j])
+              
+        return least
