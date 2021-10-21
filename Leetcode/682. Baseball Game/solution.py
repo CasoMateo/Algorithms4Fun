d3133f80dccefa3f34d8606a5c8a9561d57211ca
CasoMateo <67202211+CasoMateo@users.noreply.github.com>
2021-10-21 09:36:49 -0500

    Update solution.py

--- Leetcode/682. Baseball Game/solution.py
+++ Leetcode/682. Baseball Game/solution.py
@@ -1,19 +1,20 @@
 class Solution:
     def calPoints(self, ops: List[str]) -> int:
         
-        prev = []
+        stack = []
         
-        for i in range(len(ops)):
-          if ops[i].isdigit() or ops[i].strip('-').isdigit():
-            prev.append(int(ops[i]))
+        for operation in ops:
+       
+          if operation == 'C':
+            stack.pop()
           
-          elif ops[i] == 'C':
-            prev.pop()
+          elif operation == 'D':
+            stack.append(stack[-1] * 2)
           
-          elif ops[i] == 'D':
-            prev.append(prev[-1] * 2)
-          
-          elif ops[i] == '+': 
-            prev.append(prev[-2] + prev[-1])
+          elif operation == '+': 
+            stack.append(stack[-2] + stack[-1])
       
-        return sum(prev)
+          else: 
+            stack.append(int(operation))
+            
+        return sum(stack)
