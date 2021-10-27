99af8b3d83fd0f89e1b715dc4330f24f2c89bac5
CasoMateo <67202211+CasoMateo@users.noreply.github.com>
2021-10-27 09:01:15 -0500

    Create solution.py

---
+++ Leetcode/944. Delete Columns to Make Sorted/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minDeletionSize(self, strs: List[str]) -> int:
+        
+      remove = 0
+      
+      for i in range(len(strs[0])):       
+        for j in range(len(strs) - 1):
+          if ord(strs[j][i]) > ord(strs[j + 1][i]):
+            remove += 1 
+            break
+     
+      return remove
