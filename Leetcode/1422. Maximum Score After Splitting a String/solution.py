5e333a46598b6bd3a62aabd7b62f5506e0c129e7
CasoMateo <67202211+CasoMateo@users.noreply.github.com>
2021-11-10 20:01:27 -0600

    Update solution.py

--- Leetcode/1422. Maximum Score After Splitting a String/solution.py
+++ Leetcode/1422. Maximum Score After Splitting a String/solution.py
@@ -1,26 +1,23 @@
+
 class Solution:
     def maxScore(self, s: str) -> int:
       
-      mem0 = {}
-      cur = 0
-      
-      for i in range(len(s)):
-        if s[i] == '0':
-          cur += 1 
-        
-        mem0[i] = cur 
-      
-      mem1 = {}
+      mem = {}
       cur = 0 
       for j in range(len(s)):
         if s[len(s) - j - 1] == '1':
           cur += 1 
         
-        mem1[len(s) - j - 1] = cur
+        mem[len(s) - j - 1] = cur
      
-      score = 0
+      score = cur = 0
+      
       for k in range(len(s) - 1):
-        score = max(score, mem0[k] + mem1[k + 1])
+        if s[k] == '0':
+          cur += 1
+          
+        score = max(score, cur + mem[k + 1])
         
       return score
+      
   
