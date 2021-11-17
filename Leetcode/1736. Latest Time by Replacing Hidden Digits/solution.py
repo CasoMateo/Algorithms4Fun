4dd0bacfef987a87bc8be2c027d0bd6ed242d00c
CasoMateo <67202211+CasoMateo@users.noreply.github.com>
2021-11-17 11:18:46 -0600

    Create solution.py

---
+++ Leetcode/1736. Latest Time by Replacing Hidden Digits/solution.py
@@ -0,0 +1,31 @@
+class Solution:
+    def maximumTime(self, time: str) -> str:
+    
+      mem = {0: '2', 1: '9', 3: '5', 4: '9'}
+      new = list(time)
+      
+      if new[0] == '?':
+        prev = '2'
+      else:
+        prev = new[0]
+      
+      for i in range(len(new)):
+        if new[i] == ':':
+          continue
+          
+        if new[i] == '?':
+          if i == 0: 
+            if new[i + 1] != '?' and int(new[i + 1]) >= 4:
+              new[i] = '1'
+              continue
+            
+          elif i == 1:
+            if prev == '2':
+              new[i] = '3'
+              continue
+                
+          new[i] = mem[i]
+        
+      return ''.join(new)
+        
+        
